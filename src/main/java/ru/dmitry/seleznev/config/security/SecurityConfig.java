f1884f20cbdf49eeab3eecca32a047225c865a4f
Dmitry127 <sseleznev55@gmail.com>
2021-10-01 16:04:56 +0300

    fixes

--- src/main/java/ru/dmitry/seleznev/config/security/SecurityConfig.java
+++ src/main/java/ru/dmitry/seleznev/config/security/SecurityConfig.java
@@ -1,40 +1,41 @@
 package ru.dmitry.seleznev.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.dmitry.seleznev.config.security.handler.LoginSuccessHandler;
+import ru.dmitry.seleznev.config.handler.LoginSuccessHandler;
 import ru.dmitry.seleznev.service.UserServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private UserServiceImpl userDetailsService;
-    private LoginSuccessHandler loginSuccessHandler;
+    private final UserServiceImpl userDetailsService;
+    private final LoginSuccessHandler loginSuccessHandler;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public SecurityConfig(UserServiceImpl userDetailsService, LoginSuccessHandler loginSuccessHandler) {
+    public SecurityConfig(UserServiceImpl userDetailsService, LoginSuccessHandler loginSuccessHandler,
+                          PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
         this.loginSuccessHandler = loginSuccessHandler;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
-                .successHandler(new LoginSuccessHandler())
+                .successHandler(loginSuccessHandler)
                 .permitAll();
 
         http.logout()
@@ -46,10 +47,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login").anonymous()
                 .antMatchers("/user").access("hasAnyRole('USER', 'ADMIN')")
                 .antMatchers("/admin").access("hasRole('ADMIN')").anyRequest().authenticated();
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
